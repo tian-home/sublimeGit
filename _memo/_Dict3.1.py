2d7f09454f9d161a7218629e86af8d972eaff53a
tian-home <tian-home@outlook.com>
2023-11-03 09:26:19 +0800

    2023-1103

--- _memo/_Dict3.1.py
+++ _memo/_Dict3.1.py
@@ -545,6 +545,18 @@ kd = dict(collections.OrderedDict(sorted(dd.items(), key=lambda t: t[0])))
 #按照value排序
 vd = collections.OrderedDict(sorted(dd.items(),key=lambda t:t[1]))
 
+## dict列表排序
+# -*- coding:utf-8 -*-
+anchors = [{'title': '蒜香莲藕虾', 'mat': '用料：莲藕、虾、蒜、葱、酱油、盐、白糖、水、植物油', 'collect': 28}, {'title': '小炒金钱肚', 'mat': '用料：金钱肚、青椒、胡萝卜、洋葱、小米辣、生姜、大蒜头、油、生抽、白糖、盐、黄酒', 'collect': 98}, {'title': '手抓牛肉', 'mat': '用料：牛肉、胡萝卜、豆角、香菇、蒜、姜、蒜苗', 'collect': 19}, {'title': '茯苓姬松茸排骨汤', 'mat': '用料：姬松茸、莲子、百合、茯苓、生姜、排骨、盐、纯净水、料酒', 'collect': 92}, {'title': '凉拌娃娃菜', 'mat': '用料：娃娃菜、小米椒、蒜、醋、生抽、香油', 'collect': 95}]
+
+# anchors = sorted(anchors,key=collect)
+anchors.sort(key=lambda x:x["collect"], reverse=True)
+
+for s in anchors:
+    print(s)
+
+
+
 ## 字典合并
     
 # 两个字典
--- pyCode/_test.py
+++ pyCode/_test.py
@@ -1,9 +1,8 @@
-s_kpi_all = r'认知,考虑,首选,不考虑,最终购买,之前使用,目前使用'
-s_kpi_1 = r'认知,考虑,首选,不考虑'
-s_kpi_2 = r'最终购买,之前使用,目前使用'
+# -*- coding:utf-8 -*-
+anchors = [{'title': '蒜香莲藕虾', 'mat': '用料：莲藕、虾、蒜、葱、酱油、盐、白糖、水、植物油', 'collect': 28}, {'title': '小炒金钱肚', 'mat': '用料：金钱肚、青椒、胡萝卜、洋葱、小米辣、生姜、大蒜头、油、生抽、白糖、盐、黄酒', 'collect': 98}, {'title': '手抓牛肉', 'mat': '用料：牛肉、胡萝卜、豆角、香菇、蒜、姜、蒜苗', 'collect': 19}, {'title': '茯苓姬松茸排骨汤', 'mat': '用料：姬松茸、莲子、百合、茯苓、生姜、排骨、盐、纯净水、料酒', 'collect': 92}, {'title': '凉拌娃娃菜', 'mat': '用料：娃娃菜、小米椒、蒜、醋、生抽、香油', 'collect': 95}]
 
+# anchors = sorted(anchors,key=collect)
+anchors.sort(key=lambda x:x["collect"], reverse=True)
 
-
-l_kpi_all = s_kpi_all.split(",")
-
-print(l_kpi_all)
+for s in anchors:
+    print(s)
